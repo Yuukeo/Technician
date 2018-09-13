093799374282d553ce5ead5f893cc96c9422b1f8
Jacob Boyles <jacob.boyles.16@cnu.edu>
2018-09-13 14:05:13 -0400

    Spawning vehicle now complete, implementing blips and waypoint handling

--- Client/Client.cs
+++ Client/Client.cs
@@ -11,13 +11,13 @@ namespace Client
 {
     public class Client : BaseScript
     {
-        private static bool onDuty = false;
+        private static Vehicle _veh;
+        private static bool _onDuty = false;
         private const float StartHeading = 343f;
         private static readonly Vector3 StartJob = new Vector3(2769.38f, 1402.09f, 23.55f);
 
         private static readonly List<Vector3> Locations = new List<Vector3>
         {
-            new Vector3(2455.32f, 1603.83f, 32.73f),
             new Vector3(2557.46f, 2578.59f, 37.95f),
             new Vector3(2295.41f, 2944.09f, 46.58f),
             new Vector3(2052.17f, 3688.94f, 34.59f),
@@ -27,7 +27,10 @@ namespace Client
             new Vector3(-2542.85f, 2301.51f, 33.21f),
             new Vector3(750.23f, 1273.85f, 360.3f),
             new Vector3(1573.58f, 851.63f, 77.48f),
+            new Vector3(2455.32f, 1603.83f, 32.73f),
         };
+
+        private static List<Blip> blipList = new List<Blip>();
 
         public Client()
         {
@@ -36,17 +39,55 @@ namespace Client
 
         private static async Task HandleTechnician()
         {
-            if (!onDuty)
+            if (!_onDuty)
             {
-                onDuty = true;
                 var vehicle = await World.CreateVehicle(new Model(VehicleHash.Boxville), StartJob, StartHeading);
-                //var vehicle = Function.Call<Vehicle>(Hash.CREATE_VEHICLE, 3078201489, StartJob.X, StartJob.Y + 5f, StartJob.Z, StartHeading, true, false);
-                Game.PlayerPed.Task.EnterVehicle(vehicle, VehicleSeat.Driver, 5000);
+                vehicle.ToggleExtra(2, false);
+                vehicle.ToggleExtra(3, false);
+                vehicle.ToggleExtra(4, true);
+                Game.PlayerPed.Task.WarpIntoVehicle(vehicle, VehicleSeat.Driver);
+                _veh = vehicle;
+                _onDuty = !_onDuty;
+                 await HandleWaypoints();
             }
             else
             {
-                Screen.DisplayHelpTextThisFrame($"You may only rent one ~g~Utility Truck~s~ at a time!");
+                Game.PlayerPed.Task.LeaveVehicle(_veh, true);
+                await Delay(2500);
+                _veh.Delete();
+                _onDuty = !_onDuty;
             }
+        }
+
+        private static async Task HandleWaypoints()
+        {
+            var player = Game.PlayerPed;
+            foreach (var loc in Locations)
+            {
+                var blip = World.CreateBlip(loc);
+                blip.Color = BlipColor.White;
+                blip.IsShortRange = true;
+                blip.Sprite = (BlipSprite) 544;
+                blipList.Add(blip);
+                await Delay(10);
+            }
+
+            int i = 0;
+            while (_onDuty)
+            {
+                var currentLocation = Locations[i];
+                API.SetNewWaypoint(currentLocation.X, currentLocation.Y);
+                if (player.Position.DistanceToSquared(currentLocation) < 16f)
+                {
+                    await HandleJob(currentLocation);
+                }
+                i++;
+            }
+        }
+
+        private static async Task HandleJob(Vector3 currentLocation)
+        {
+            World.DrawMarker(MarkerType.ChevronUpx3, currentLocation, Vector3.Zero, new Vector3(0f, 0.5f, 0f), Vector3.One * 5f, Color.FromArgb(255, 0, 100));
         }
 
         private static async Task OnTick()
@@ -59,7 +100,7 @@ namespace Client
                     World.DrawMarker(MarkerType.HorizontalCircleFat, StartJob, Vector3.Zero, new Vector3(0f, 0.5f, 0f), Vector3.One * 5f, Color.FromArgb(0, 255, 255));
                     if (distance < 5f)
                     {
-                        Screen.DisplayHelpTextThisFrame($"Press ~INPUT_CONTEXT~ to retrieve a ~g~Utility Truck~s~");
+                        Screen.DisplayHelpTextThisFrame($"Press ~INPUT_CONTEXT~ to {(_onDuty ? "store your" : "retrieve a")} ~g~Utility Truck~s~");
                         if (Game.IsControlJustPressed(0, Control.Context))
                         {
                             await HandleTechnician();
