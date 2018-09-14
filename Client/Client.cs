77284ed4a19970e1d2b9475a5f52f20092e1b4d8
Jacob Boyles <jacob.boyles.16@cnu.edu>
2018-09-13 21:53:49 -0400

    Lost of code added, half of it doesn't work but the base is getting there

--- Client/Client.cs
+++ Client/Client.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.Threading.Tasks;
 using CitizenFX.Core;
 using CitizenFX.Core.Native;
-using CitizenFX.Core.NaturalMotion;
 using CitizenFX.Core.UI;
 
 namespace Client
@@ -12,13 +11,14 @@ namespace Client
     public class Client : BaseScript
     {
         private static Vehicle _veh;
-        private static bool _onDuty = false;
+        private static bool _onDuty;
+        private static bool _coolDown;
+        private static int _coolDownTime;
         private const float StartHeading = 343f;
         private static readonly Vector3 StartJob = new Vector3(2769.38f, 1402.09f, 23.55f);
 
         private static readonly List<Vector3> Locations = new List<Vector3>
         {
-            new Vector3(2557.46f, 2578.59f, 37.95f),
             new Vector3(2295.41f, 2944.09f, 46.58f),
             new Vector3(2052.17f, 3688.94f, 34.59f),
             new Vector3(3438.59f, 3749.54f, 30.51f),
@@ -27,19 +27,37 @@ namespace Client
             new Vector3(-2542.85f, 2301.51f, 33.21f),
             new Vector3(750.23f, 1273.85f, 360.3f),
             new Vector3(1573.58f, 851.63f, 77.48f),
-            new Vector3(2455.32f, 1603.83f, 32.73f),
+            new Vector3(2557.46f, 2578.59f, 37.95f),
         };
 
         private static List<Blip> blipList = new List<Blip>();
 
         public Client()
         {
+            var blip = World.CreateBlip(StartJob);
+            blip.Name = "Power Technician Job";
+            blip.IsShortRange = true;
+            blip.Sprite = (BlipSprite) 544;
+            blip.Color = BlipColor.TrevorOrange;
+            EventHandlers["Technician.Cooldown"] += new Action<bool>(SetCooldown);
+            EventHandlers["Technician.Remaining"] += new Action<int>(SetRemaining);
             Tick += OnTick;
         }
 
-        private static async Task HandleTechnician()
+        private static void SetCooldown(bool cooldown)
         {
-            if (!_onDuty)
+            _coolDown = cooldown;
+        }
+
+        private static void SetRemaining(int cooldown)
+        {
+            _coolDownTime = cooldown;
+        }
+
+        private static async void HandleTechnician()
+        {
+            TriggerServerEvent("Technician.GetCooldownBool");
+            if (!_onDuty && !_coolDown)
             {
                 var vehicle = await World.CreateVehicle(new Model(VehicleHash.Boxville), StartJob, StartHeading);
                 vehicle.ToggleExtra(2, false);
@@ -47,20 +65,31 @@ namespace Client
                 vehicle.ToggleExtra(4, true);
                 Game.PlayerPed.Task.WarpIntoVehicle(vehicle, VehicleSeat.Driver);
                 _veh = vehicle;
-                _onDuty = !_onDuty;
-                 await HandleWaypoints();
+                _onDuty = true;
+                TriggerServerEvent("Technician.JobStarted");
+                HandleWaypoints();
             }
-            else
+            else if (_veh.Position.DistanceToSquared(StartJob) < 10f)
             {
                 Game.PlayerPed.Task.LeaveVehicle(_veh, true);
                 await Delay(2500);
                 _veh.Delete();
-                _onDuty = !_onDuty;
+                _onDuty = false;
+            }
+            else if (_coolDown)
+            {
+                TriggerServerEvent("Technician.Remaining");
+                Screen.ShowNotification($"You must wait {_coolDownTime} seconds before going back on duty.");
+            }
+            else
+            {
+                Screen.ShowNotification($"Your vehicle must be close to the garage!");
             }
         }
 
-        private static async Task HandleWaypoints()
+        private static async void HandleWaypoints()
         {
+            blipList.Clear();
             var player = Game.PlayerPed;
             foreach (var loc in Locations)
             {
@@ -75,19 +104,29 @@ namespace Client
             int i = 0;
             while (_onDuty)
             {
+                TriggerEvent("chatMessage", "In while loop");
                 var currentLocation = Locations[i];
+                var distance = player.Position.DistanceToSquared(currentLocation);
                 API.SetNewWaypoint(currentLocation.X, currentLocation.Y);
-                if (player.Position.DistanceToSquared(currentLocation) < 16f)
+                if (distance < 400f)
                 {
+                    TriggerEvent("chatMessage", "In if statement");
                     await HandleJob(currentLocation);
                 }
                 i++;
             }
+            TriggerEvent("chatMessage", "Out of while loop");
         }
 
         private static async Task HandleJob(Vector3 currentLocation)
         {
-            World.DrawMarker(MarkerType.ChevronUpx3, currentLocation, Vector3.Zero, new Vector3(0f, 0.5f, 0f), Vector3.One * 5f, Color.FromArgb(255, 0, 100));
+            TriggerEvent("chatMessage", "Creating marker");
+            World.DrawMarker(MarkerType.HorizontalCircleFat, currentLocation, Vector3.Zero, new Vector3(0f, 0.5f, 0f), Vector3.One * 5f, Color.FromArgb(0, 255, 255));
+            Screen.DisplayHelpTextThisFrame("Test");
+            while (true)
+            {
+                await Delay(10);
+            }
         }
 
         private static async Task OnTick()
@@ -98,12 +137,12 @@ namespace Client
                 if (distance < 400)
                 {
                     World.DrawMarker(MarkerType.HorizontalCircleFat, StartJob, Vector3.Zero, new Vector3(0f, 0.5f, 0f), Vector3.One * 5f, Color.FromArgb(0, 255, 255));
-                    if (distance < 5f)
+                    if (distance < 5)
                     {
                         Screen.DisplayHelpTextThisFrame($"Press ~INPUT_CONTEXT~ to {(_onDuty ? "store your" : "retrieve a")} ~g~Utility Truck~s~");
                         if (Game.IsControlJustPressed(0, Control.Context))
                         {
-                            await HandleTechnician();
+                            HandleTechnician();
                         }
                     }
                 }
